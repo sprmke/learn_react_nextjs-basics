e5b4f17628ebe4dfab6ca0b7ba61f16eb6a875b0
Michael Manlulu <michael.manlulu@myresman.com>
2022-09-22 09:48:56 +0800

    327. linking between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <div>NewsPage</div>;
+  return (
+    <>
+      <h1>NewsPage</h1>
+      <ul>
+        <li>
+          <Link href='/news/1'>Link 1</Link>
+        </li>
+        <li>
+          <Link href='/news/2'>Link 2</Link>
+        </li>
+        <li>
+          <Link href='/news/3'>Link 3</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
